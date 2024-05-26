b78595af81cbfdb2eda57363d81056b91458e7e2
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-05-26 10:22:18 +0300

    add class Game
    Создаем класс game для управления игрой

--- main.py
+++ main.py
@@ -18,3 +18,32 @@ class Hero:
     def __str__(self):
         return f"{self.name}: здоровье={self.health}, сила удара={self.attack_power}"
 
+
+class Game:
+    def __init__(self, player_name):
+        """Инициализирует игру с игроком и компьютером."""
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
+
+    def start(self):
+        """Начинает игру, чередует ходы игрока и компьютера, пока один из героев не умрет."""
+        print("Начало игры!")
+        print(self.player)
+        print(self.computer)
+
+        while self.player.is_alive() and self.computer.is_alive():
+            # Ход игрока
+            damage = self.player.attack(self.computer)
+            print(f"{self.player.name} атакует {self.computer.name} и наносит {damage} урона.")
+            print(self.computer)
+            if not self.computer.is_alive():
+                print(f"{self.computer.name} повержен! {self.player.name} победил!")
+                break
+
+            # Ход компьютера
+            damage = self.computer.attack(self.player)
+            print(f"{self.computer.name} атакует {self.player.name} и наносит {damage} урона.")
+            print(self.player)
+            if not self.player.is_alive():
+                print(f"{self.player.name} повержен! {self.computer.name} победил!")
+                break
